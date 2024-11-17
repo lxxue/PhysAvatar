6c53c59be20b596d9c78e48bd7969365458d220d
Lixin Xue <lixin.xue@inf.ethz.ch>
2024-11-17 11:38:13 +0100

    [10-17] change mesh save path

--- create_meshes.py
+++ create_meshes.py
@@ -7,7 +7,7 @@ import os
 from tqdm import trange
 
 def create_meshes(exp, seq, num_frames, md):
-    os.makedirs(f"{root}/{seq}/PhysAvatar/meshes", exist_ok=True)
+    os.makedirs(f"{root}/{seq}/PhysAvatar/meshes_{exp}", exist_ok=True)
     for i in trange(num_frames):
         frame_idx = md["fn"][i][0].split("/")[-1].split(".")[0]
         params_t = np.load(f"./output/{exp}/{seq}/params_{i}.npz")
@@ -15,7 +15,7 @@ def create_meshes(exp, seq, num_frames, md):
         faces = params_t["faces"]
         vertex_colors = params_t["rgb_colors"]
         mesh = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_colors=vertex_colors)
-        mesh.export(f"{root}/{seq}/PhysAvatar/meshes/{frame_idx}.obj")
+        mesh.export(f"{root}/{seq}/PhysAvatar/meshes_{exp}/{frame_idx}.obj")
 
     print("Scene data loaded")
     return
@@ -24,11 +24,13 @@ def create_meshes(exp, seq, num_frames, md):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--exp_name", type=str, required=True)
     parser.add_argument("--seq", type=str, required=True)
     args = parser.parse_args()
     root="/home/lixin/mount/scratch/lixin/GSTAR"
     seq = args.seq
-    exp_name = "exp1_gstar"
+    # exp_name = "exp1_gstar"
+    exp_name = args.exp_name
     md = json.load(open(f"{root}/{seq}/Dynamic3DGS/train_meta.json", 'r'))
     num_frames = len(md['fn'])
     create_meshes(exp_name, seq, num_frames, md)
